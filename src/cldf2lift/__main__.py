455e63e8b057e6798b05998c9b3616992230d18d
Johannes Englisch <englisch@shh.mpg.de>
2021-03-31 10:52:24 +0200

    formatting

--- src/cldf2lift/__main__.py
+++ src/cldf2lift/__main__.py
@@ -9,10 +9,9 @@ from pycldf import Dataset
 def main():
     cldf = Dataset.from_metadata('tests/test_data/Dictionary-metadata.json')
     lift = ET.Element('lift')
-    print(str(ET.tostring(
-        lift,
-        encoding='UTF-8',
-        xml_declaration=True), encoding='utf-8'))
+    print(str(
+        ET.tostring(lift, encoding='UTF-8', xml_declaration=True),
+        encoding='utf-8'))
 
 
 if __name__ == '__main__':
