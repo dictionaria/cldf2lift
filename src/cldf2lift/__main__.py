84f52c4d8d69c553a6eb87e8a4429f3376233deb
Johannes Englisch <englisch@shh.mpg.de>
2021-03-31 11:03:53 +0200

    read entries

--- src/cldf2lift/__main__.py
+++ src/cldf2lift/__main__.py
@@ -8,7 +8,20 @@ from pycldf import Dataset
 
 def main():
     cldf = Dataset.from_metadata('tests/test_data/Dictionary-metadata.json')
-    lift = ET.Element('lift')
+    # TODO add proper 'lang' attribute (wants two-letter isocode?)
+    lang = 'und-Latn'
+
+    lift = ET.Element('lift', attrib={'lang': lang})
+    for entry in cldf['EntryTable']:
+        id_ = entry.get('ID')
+        lx = entry.get('Headword')
+        ps = entry.get('Part_Of_Speech')
+        # TODO proper error handling
+        assert id_ and lx, 'invalid entry'
+        xml_entry = ET.SubElement(lift, 'entry', attrib={'id': id_})
+        xml_lexunit = ET.SubElement(xml_entry, 'lexical-unit')
+        ET.SubElement(xml_lexunit, 'form', text=lx, attrib={'lang': lang})
+
     print(str(
         ET.tostring(lift, encoding='UTF-8', xml_declaration=True),
         encoding='utf-8'))
