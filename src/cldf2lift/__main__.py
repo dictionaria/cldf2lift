cfd928d73d296c32f8bfc36470bf6907af70db33
Johannes Englisch <englisch@shh.mpg.de>
2021-03-31 11:37:43 +0200

    sense info

--- src/cldf2lift/__main__.py
+++ src/cldf2lift/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+from collections import OrderedDict
 from xml.etree import ElementTree as ET
 
 from pycldf import Dataset
@@ -11,16 +11,38 @@ def main():
     # TODO add proper 'lang' attribute (wants two-letter isocode?)
     lang = 'und-Latn'
 
+    entries = cldf['EntryTable']
+    senses = OrderedDict()
+    for sense in cldf['SenseTable']:
+        entry_id = sense.get('Entry_ID')
+        # TODO proper error handling
+        assert entry_id, 'invalid sense'
+        assert sense.get('ID') and sense.get('Description'), 'invalid sense'
+        if entry_id not in senses:
+            senses[entry_id] = []
+        senses[entry_id].append(sense)
+
     lift = ET.Element('lift', attrib={'lang': lang})
-    for entry in cldf['EntryTable']:
-        id_ = entry.get('ID')
+    for entry in entries:
+        entry_id = entry.get('ID')
         lx = entry.get('Headword')
         ps = entry.get('Part_Of_Speech')
         # TODO proper error handling
-        assert id_ and lx, 'invalid entry'
-        xml_entry = ET.SubElement(lift, 'entry', attrib={'id': id_})
+        assert entry_id and lx, 'invalid entry'
+        xml_entry = ET.SubElement(lift, 'entry', attrib={'id': entry_id})
         xml_lexunit = ET.SubElement(xml_entry, 'lexical-unit')
-        ET.SubElement(xml_lexunit, 'form', text=lx, attrib={'lang': lang})
+        xml_form = ET.SubElement(xml_lexunit, 'form', attrib={'lang': lang})
+        ET.SubElement(xml_form, 'text', text=lx)
+        for sense in senses[entry_id]:
+            sense_id = sense['ID']
+            de = sense['Description']
+            xml_sense_id = '{}-{}'.format(entry_id, sense_id)
+            xml_sense = ET.SubElement(
+                xml_entry, 'sense', attrib={'id': xml_sense_id})
+            ET.SubElement(xml_sense, 'grammatical-info', attrib={'type': ps})
+            xml_de = ET.SubElement(xml_sense, 'definition')
+            xml_de_form = ET.SubElement(xml_de, 'form', attrib={'lang': 'en'})
+            ET.SubElement(xml_de_form, 'text', text=de)
 
     print(str(
         ET.tostring(lift, encoding='UTF-8', xml_declaration=True),
