6b313bfac418900df33f926c5e1dce7f7996b6fa
Johannes Englisch <englisch@shh.mpg.de>
2021-03-31 10:52:06 +0200

    xml declaration

--- src/cldf2lift/__main__.py
+++ src/cldf2lift/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+import sys
 from xml.etree import ElementTree as ET
 
 from pycldf import Dataset
@@ -9,7 +9,10 @@ from pycldf import Dataset
 def main():
     cldf = Dataset.from_metadata('tests/test_data/Dictionary-metadata.json')
     lift = ET.Element('lift')
-    print(ET.tostring(lift))
+    print(str(ET.tostring(
+        lift,
+        encoding='UTF-8',
+        xml_declaration=True), encoding='utf-8'))
 
 
 if __name__ == '__main__':
